677d58b6b8a7fe3a02f6b2ae977f77a1700fdd59
HieuVM <hieuvm@topcv.vn>
2025-05-04 11:17:03 +0700

    Public link

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,11 +45,11 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => public_path('storage'),
-            'url' => env('APP_URL') . '/storage',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'throw' => false,
+            'throw'      => false,
         ],
 
         's3' => [
