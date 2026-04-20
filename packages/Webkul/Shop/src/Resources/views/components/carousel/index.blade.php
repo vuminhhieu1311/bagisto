47215320599216a00dc0995cca5ae3b425c1b3f7
HieuVM <hieuvm@topcv.vn>
2026-04-20 12:06:07 +0700

    Update

--- packages/Webkul/Shop/src/Resources/views/components/carousel/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/carousel/index.blade.php
@@ -2,7 +2,7 @@
 
 <v-carousel :images="{{ json_encode($options['images'] ?? []) }}">
     <div class="overflow-hidden">
-        <div class="shimmer aspect-[2.743/1] max-h-screen w-screen"></div>
+        <div class="shimmer aspect-[1820/900] max-h-screen w-screen"></div>
     </div>
 </v-carousel>
 
@@ -24,7 +24,7 @@
                     ref="slide"
                 >
                     <x-shop::media.images.lazy
-                        class="aspect-[2.743/1] max-h-full w-full max-w-full select-none transition-transform duration-300 ease-in-out"
+                        class="aspect-[1820/900] max-h-full w-full max-w-full select-none transition-transform duration-300 ease-in-out"
                         ::lazy="false"
                         ::src="image.image"
                         ::srcset="image.image + ' 1920w, ' + image.image.replace('storage', 'cache/large') + ' 1280w,' + image.image.replace('storage', 'cache/medium') + ' 1024w, ' + image.image.replace('storage', 'cache/small') + ' 525w'"
